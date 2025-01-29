40aa9a9f8ddd33a877292146006cddab0a3b88e6
Sergey Popov <psamaestro@yandex.ru>
2025-01-29 18:22:48 +0300

    Изменен index в dist

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -4,18 +4,18 @@ import style from "./style.module.css";
 import { Link } from "react-router-dom";
 
 type Props = {
-    data: Bean;
+  data: Bean;
 };
 
-const Card: FC<Props> = ({data}) => {
-    return (
+const Card: FC<Props> = ({ data }) => {
+  return (
     <Link to={`/react-project/bean/${data.beanId}`} className={style.card}>
-        <img src = {data.imageUrl} alt="" />
-        <h2>{data.flavorName}</h2>
-        <p>{data.description}</p>
-        {data.sugarFree ? <p>Not sugar</p> : <p>With sugar</p>}
+      <img src={data.imageUrl} alt="" />
+      <h2>{data.flavorName}</h2>
+      <p>{data.description}</p>
+      {data.sugarFree ? <p>Not sugar</p> : <p>With sugar</p>}
     </Link>
-    );
+  );
 };
 
 export default Card;
