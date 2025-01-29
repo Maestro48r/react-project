5b06c1e6bdc3d2d9fd2778238966bd5bd1c75cc4
Sergey Popov <psamaestro@yandex.ru>
2025-01-29 19:24:18 +0300

    Добавил allow

--- src/components/Nav/index.tsx
+++ src/components/Nav/index.tsx
@@ -4,17 +4,17 @@ import style from "./style.module.css";
 import { Link } from "react-router-dom";
 
 const Nav = () => {
-    const [isModal, setIsModal] = useState(true);
-    return (
-        <nav className={style.container}>
-                <Link to="/react-project//beans">Beans</Link>
-                <Link to="/facts">Facts</Link>
-                <Link to="/recipies">Recipies</Link>
-                <Link to="/combinations">Combinations</Link>
-                <Link to="/history">History</Link>
-                {<Modal isModal= {isModal} onClick={() => setIsModal(false)} />}
-        </nav>
-    );
+  const [isModal, setIsModal] = useState(true);
+  return (
+    <nav className={style.container}>
+      <Link to="/react-project//beans">Beans</Link>
+      <Link to="/facts">Facts</Link>
+      <Link to="/recipies">Recipies</Link>
+      <Link to="/combinations">Combinations</Link>
+      <Link to="/history">History</Link>
+      {<Modal isModal={isModal} onClick={() => setIsModal(false)} />}
+    </nav>
+  );
 };
 
 export default Nav;
